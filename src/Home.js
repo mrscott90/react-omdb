3564bb1f08628f65fbaf29c061db08557d16b7e7
Matthew Scott <matthewruelscott@gmail.com>
2016-08-03 13:53:16 -0400

    App working

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ class Home extends Component{
   render(){
     return(
       <div style={styles.transparentBg} className="jumbotron col-sm-12 text-center">
-        <h1><a href="/">OMDB React</a></h1>
+        <h1><a href="/">Movie Search</a></h1>
         <div className="col-sm-12" style={styles.space}>
           <SearchContainer />
         </div>
