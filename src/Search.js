a63054c56dbbe2d629fb5e9652de22451667e25e
Matthew Scott <matthewruelscott@gmail.com>
2016-08-03 12:36:27 -0400

    App working

---
+++ src/Search.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import styles from './styles/index'
+
+class Search extends Component{
+  render(){
+    let {handleSearchInput, handleSubmitQuery, query} = this.props
+    return(
+      <div style={styles.transparentBg} className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <div className="col-sm-12">
+          <form onSubmit={(e) => handleSubmitQuery(e)}>
+            <div className="form-group">
+              <input
+                onChange={(e) => handleSearchInput(e)}
+                value={query}
+                type="text"
+                placeholder="Enter a Film Title..." />
+            </div>
+          <div className="form-group col-sm-4 col-sm-offset-4">
+            <button
+              className="btn btn-block btn-primary"
+              type="submit">Search!</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default Search
---
+++ src/SearchContainer.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import Search from './Search'
+import Results from './Results'
+import {queryOmdb} from './Utils'
+
+class SearchContainer extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      query: '',
+      hasSearched: false,
+      movies: [],
+    }
+  }
+  onSearchInput(e){
+    this.setState({
+      query: e.target.value,
+    })
+  }
+  onSubmitQuery(e){
+    e.preventDefault();
+    let component = this
+    queryOmdb(this.state.query).then( data => {
+      component.setState({
+        query: '',
+        hasSearched: true,
+        movies: data,
+      })
+    })
+  }
+
+  render(){
+    if(this.state.hasSearched){
+      return(
+        <Results movies={this.state.movies} />
+      )
+    } else{
+      return(
+        <Search
+        handleSearchInput={(e) => this.onSearchInput(e)}
+        handleSubmitQuery={(e) => this.onSubmitQuery(e)}
+        query={this.state.query} />
+      )
+    }
+  }
+}
+
+export default SearchContainer
